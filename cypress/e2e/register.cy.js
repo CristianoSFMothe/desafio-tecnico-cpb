eb76f7b0e1f576a22d747f1304fa206f3653a3c8
Cristiano Ferreira Mothe <cristianodevsystem@gmail.com>
2025-04-17 02:41:54 -0300

    feat: implement comprehensive club registration process with dynamic data generation

--- cypress/e2e/register.cy.js
+++ cypress/e2e/register.cy.js
@@ -1,44 +1,9 @@
-import { faker } from "@faker-js/faker";
-
-const fakerBR = require('faker-br');
-const { generateCNPJ } = require("../support/utils/cnpj-generator.utils");
-const { generateAcronym } = require("../support/utils/generator-sigla.utils")
-const { generateRandomDate } = require("../support/utils/generate-random-date")
-
-faker.location = 'pt_BR'
-
-const CNPJ = generateCNPJ();
-const clubName = faker.company.name();
-const clubSigla = generateAcronym(clubName, 1)
-const email = faker.internet.email()
-const date = generateRandomDate()
-const phone = faker.phone.number('(##) #####-####').replace(/\s*x\d+/g, '');
-const cep = fakerBR.address.zipCode('#####-###');
-const address = fakerBR.address.streetAddress();
-const number = faker.number.int({ max: 999 });
-const cpf = fakerBR.br.cpf()
-const name = faker.person.fullName()
-const emailPresident = faker.internet.email()
-const cpfDirector = fakerBR.br.cpf()
-const nameDirector = faker.person.fullName()
-
 describe('Acessar a página de registro de clubes', () => {
   beforeEach(() => {
-    cy.login()
-  })
+    cy.login();
+  });
+
   it('Deve registar um clube com sucesso', () => {
-    // DADOS DO CLUBE
-    cy.fillInClubDetails(CNPJ, clubName, clubSigla, email, date, phone)
-
-
-    // DADOS DO ENDEREÇO    
-    cy.fillAddress(cep, number, address, 'Centro', 'Rio de Janeiro', 'Rio de Janeiro')
-
-    // DADOS DA PRESIDÊNCIA
-    cy.fillFieldsPresident(cpf, name, emailPresident, date, date, date)
-
-    // DADOS DO DIRETOR
-    cy.fillFieldsDirector(cpfDirector, nameDirector)
-    cy.markModality()
-  })
-})
+    cy.registerClub();
+  });
+});
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,54 +1,72 @@
-import login from '../support/pages/loginPage'
-import registerPage from './pages/registerPage';
+import login from '../support/pages/loginPage';
+import registerPage from '../support/pages/registerPage';
+import { faker } from "@faker-js/faker";
+const fakerBR = require('faker-br');
+const { generateCNPJ } = require("../support/utils/cnpj-generator.utils");
+const { generateAcronym } = require("../support/utils/generator-sigla.utils");
+const { generateRandomDate } = require("../support/utils/generate-random-date");
+
+faker.locale = 'pt_BR';
 
 Cypress.Commands.add('login', () => {
-  login.accessPage()
-  login.validateLoginPage()
-  login.clickButton()
+  login.accessPage();
+  login.validateLoginPage();
+  login.clickButton();
 });
 
-Cypress.Commands.add('fillInClubDetails', (CNPJ, clubName, sigla, email, date, phone) => {
-  registerPage.validateRegisterPage()
-  registerPage.clickAcceptTermsButton()
-  registerPage.fieldCNPJClub(CNPJ)
-  registerPage.clickButtonSearchCNPJ()
-  registerPage.fieldName(clubName)
-  registerPage.fieldAcronym(sigla)
-  registerPage.fieldEmail(email)
-  registerPage.fieldDateFoundation(date)
-  registerPage.fielPhone(phone)
-});
+Cypress.Commands.add('registerClub', () => {
+  // Geração de dados dinâmica
+  const CNPJ = generateCNPJ();
+  const clubName = faker.company.name();
+  const clubSigla = generateAcronym(clubName, 1);
+  const email = faker.internet.email();
+  const date = generateRandomDate();
+  const phone = faker.phone.number('(##) #####-####').replace(/\s*x\d+/g, '');
+  const cep = fakerBR.address.zipCode('#####-###');
+  const address = fakerBR.address.streetAddress();
+  const number = faker.number.int({ max: 999 });
+  const cpf = fakerBR.br.cpf();
+  const name = faker.person.fullName();
+  const emailPresident = faker.internet.email();
+  const cpfDirector = fakerBR.br.cpf();
+  const nameDirector = faker.person.fullName();
 
+  // Dados do Clube
+  registerPage.validateRegisterPage();
+  registerPage.clickAcceptTermsButton();
+  registerPage.fieldCNPJClub(CNPJ);
+  registerPage.clickButtonSearchCNPJ();
+  registerPage.fieldName(clubName);
+  registerPage.fieldAcronym(clubSigla);
+  registerPage.fieldEmail(email);
+  registerPage.fieldDateFoundation(date);
+  registerPage.fielPhone(phone);
 
+  // Dados de Endereço
+  registerPage.fielCep(cep);
+  registerPage.fielAddress(address);
+  registerPage.fielAddressNumber(number);
+  registerPage.fielNeighborhood('Centro');
+  registerPage.selectState('Rio de Janeiro');
+  registerPage.selectCity('Rio de Janeiro');
 
-Cypress.Commands.add('fillAddress', (cep, address, number, neighborhood, state, city) => {
-  registerPage.fielCep(cep)
-  registerPage.fielAddress(address)
-  registerPage.fielAddressNumber(number)
-  registerPage.fielNeighborhood(neighborhood)
-  registerPage.selectState(state)
-  registerPage.selectCity(city)
-});
+  // Presidente
+  registerPage.fieldCpf(cpf);
+  registerPage.clickButtonSearchCPF();
+  registerPage.fieldNamePresident(name);
+  registerPage.fieldEmail(emailPresident);
+  registerPage.fieldDateStartPresident(date);
+  registerPage.fieldDateElectionPresident(date);
+  registerPage.fieldDateEndPresident(date);
 
+  // Diretor
+  registerPage.fieldCpfDirector(cpfDirector);
+  registerPage.clickButtonSearchCPFDirector();
+  registerPage.fieldNameDirector(nameDirector);
 
-Cypress.Commands.add('fillFieldsPresident', (cpf, name, email, dateStar, dateElection, dateEnd) => {
-  registerPage.fieldCpf(cpf)
-  registerPage.clickButtonSearchCPF()
-  registerPage.fieldNamePresident(name)
-  registerPage.fieldEmail(email)
-  registerPage.fieldDateStartPresident(dateStar)
-  registerPage.fieldDateElectionPresident(dateElection)
-  registerPage.fieldDateEndPresident(dateEnd)
+  // Modalidade
+  registerPage.modality();
 
-});
-
-Cypress.Commands.add('fillFieldsDirector', (cpf, name) => {
-  registerPage.fieldCpfDirector(cpf)
-  registerPage.clickButtonSearchCPFDirector()
-  registerPage.fieldNameDirector(name)
-});
-
-Cypress.Commands.add('markModality', () => {
-  registerPage.modality()
-  // registerPage.saveDataClub()
+  // Salvar Clube
+  registerPage.saveDataClub();
 });
